11934601a9b19a03bf5f7ce4777107b3f4880c3c
zengrongchang <zengrongchang@dasijiaoyu.com>
2020-06-16 15:56:01 +0800

    excel

--- verb_frequency.py
+++ verb_frequency.py
@@ -1,34 +1,35 @@
+import re
 from collections import Counter
 import openpyxl
-import xlrd
-import re
-from xlutils.copy import copy as xl_copy
 
 excel_path = input("excel路径: ")
 sheet_index = input("sheet序号: ")
 wc_col = input("统计列: ")
 
-data = xlrd.open_workbook(excel_path)
-table = data.sheet_by_index(int(sheet_index) - 1)
+# excel_path = "C:/Users/james/Desktop/a.xlsx"
+# sheet_index = "1"
+# wc_col = "1"
+
+book = openpyxl.load_workbook(excel_path)
+sheet = book.worksheets[int(sheet_index) - 1]
 speech = []
-for row in range(table.nrows):
+for row in range(sheet.max_row):
     value = re.sub(r'!|@|#|$|%|^|&|\*|\(|\)|[|]|{|}|"|,|\?|\.|\'|-|_|\d|:|？|；|，|。', " ",
-                   table.cell(row, int(wc_col) - 1).value.lower())
+                   sheet.cell(row + 1, int(wc_col)).value.lower())
     speech.extend(value.split())
 
 wd = Counter(speech)
 
-wc_data = xl_copy(data)
-wc_sheet = wc_data.add_sheet('词频统计')
-wc_sheet.write(0, 0, "单词")
-wc_sheet.write(0, 1, "频率")
-item_size = wd.items()
+wc_sheet = book.create_sheet('词频统计')
+wc_sheet.cell(1, 1, "单词")
+wc_sheet.cell(1, 2, "频率")
 
-i = 1
+item_size = wd.items()
+i = 2
 for key, value in sorted(wd.items(), key=lambda kv: (kv[0], kv[1])):
-    wc_sheet.write(i, 0, key)
-    wc_sheet.write(i, 1, value)
+    wc_sheet.cell(i, 1, key)
+    wc_sheet.cell(i, 2, value)
     i = i + 1
 
-wc_data.save(excel_path)
+book.save(excel_path)
 input("统计完成，按enter退出！")
